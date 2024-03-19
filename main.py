27e1a1d66b4cc6d89dc59ac070a6492700b3984b
kaseywolff <kaseywolff5@gmail.com>
2024-03-19 16:19:48 -0500

    running hiker works but  need third img

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 pygame.init()
 
 # global constants
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 900
 SCREEN_WIDTH = 1100
-SCREEN = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT) # defines display where game will be shown
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # defines display where game will be shown
 
 # LOAD IN IMAGES - HIKER
 RUNNING = [pygame.image.load(os.path.join('assets/hiker', 'hiker-run1.png')), 
@@ -28,6 +28,72 @@ JUMPING = pygame.image.load(os.path.join('assets/hiker', 'hiker-jump.png'))
 # # BACKGROUND
 # CLOUD = pygame.image.load(os.path.join('assets/background', 'cloud.png'))
 # BACKGROUND = pygame.image.load(os.path.join('assets/background', 'track.png'))
+
+
+class Hiker:
+  # position of hiker - remains stationary throughout game
+  X_POS = 80
+  Y_POS = 310
+
+  def __init__(self):
+    # self.duck_img = DUCKING
+    self.run_img = RUNNING
+    self.jump_img = JUMPING
+
+    self.hiker_duck = False
+    self.hiker_run = True
+    self.hiker_jump = False
+
+    self.step_index = 0
+    self.image = self.run_img[0]
+    # hit box
+    self.hiker_rect = self.image.get_rect()
+    self.hiker_rect.x = self.X_POS
+    self.hiker_rect.y = self.Y_POS
+
+
+  def update(self, userInput):
+    if self.hiker_duck:
+      self.duck()
+    if self.hiker_run:
+      self.run()
+    if self.hiker_jump:
+      self.jump()
+
+    # step index to help animate hiker
+    if self.step_index >= 10:
+      self.step_index = 0
+    
+    # jumping and ducking functionality
+    if userInput[pygame.K_UP] and not self.hiker_jump:
+      self.hiker_duck = False
+      self.hiker_run = False
+      self.hiker_jump = True
+    elif userInput[pygame.K_DOWN] and not self.hiker_jump:
+      self.hiker_duck = True
+      self.hiker_run = False
+      self.hiker_jump = False
+    elif not (self.hiker_jump or userInput[pygame.K_DOWN]):
+      self.hiker_duck = False
+      self.hiker_run = True
+      self.hiker_jump = False
+
+  def duck(self):
+    pass
+
+  # run animation
+  def run(self):
+    self.image = self.run_img[self.step_index // 5]
+    self.hiker_rect = self.image.get_rect()
+    self.hiker_rect.x = self.X_POS
+    self.hiker_rect.y = self.Y_POS
+    self.step_index += 1
+
+  def jump(self):
+    pass
+
+  def draw(self, SCREEN):
+    SCREEN.blit(self.image, (self.hiker_rect.x, self.hiker_rect.y))
 
 
 def main():
